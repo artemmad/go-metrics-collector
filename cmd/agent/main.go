d3c69c9f3ae20f2a3871c4bbbdc98700a1a1efeb
Artem Makarevich <artemmakarevich1997@gmail.com>
2025-05-06 21:47:57 +0400

    iter2

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	agent "github.com/artemmad/go-metrics-collector/internal/Agent"
+	"time"
+)
+
+const (
+	pollInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
+
+func main() {
+	go func() {
+		for {
+			agent.UpdateMetrics()
+			time.Sleep(pollInterval)
+		}
+	}()
+	for {
+		agent.ReportMetrics()
+		time.Sleep(reportInterval)
+	}
+}
