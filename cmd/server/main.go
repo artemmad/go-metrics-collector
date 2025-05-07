44a994e3d0166c753a543d939841e103714c57fc
Artem Makarevich <artemmakarevich1997@gmail.com>
2025-05-08 00:02:42 +0400

    iter3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"github.com/artemmad/go-metrics-collector/internal/Server/handlers"
 	"github.com/artemmad/go-metrics-collector/internal/Server/storage"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"net/http"
+	"time"
 )
 
 func main() {
 	store := storage.NewMemStorage()
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
 
-	mux.HandleFunc("POST /update/", handlers.MetricCalc(store))
-	mux.HandleFunc("GET /", handlers.MetricList(store))
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		http.Error(writer, "404 page not found", http.StatusNotFound)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	//mux := http.NewServeMux()
+
+	//mux.HandleFunc("POST /update/", handlers.MetricCalc(store))
+	//mux.HandleFunc("GET /", handlers.MetricList(store))
+	//mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+	//	http.Error(writer, "404 page not found", http.StatusNotFound)
+	//})
+	r.Get("/", handlers.MetricList(store))
+	r.Route("/update", func(r chi.Router) {
+		r.Post("/{metricType}/{metricName}/{value}", handlers.MetricCalc(store))
+	})
+	r.Route("/value", func(r chi.Router) {
+		r.Get("/{metricType}/{metricName}", handlers.GetOneMetric(store))
 	})
 
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
