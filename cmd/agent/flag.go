566d3b0619b13186e54c0db4538f04b7ac7b312c
Artem Makarevich <artemmakarevich1997@gmail.com>
2025-05-11 00:24:38 +0400

    iter4

--- cmd/agent/flag.go
+++ cmd/agent/flag.go
@@ -17,7 +17,7 @@ const (
 )
 
 func configFlags() {
-	flag.StringVar(&serverAddress, "a", ":8080", "The address to bind the server to, ex. localhost:8080")
+	flag.StringVar(&serverAddress, "a", "http://localhost:8080", "The address to bind the server to, ex. http://localhost:8080")
 	flag.DurationVar(&reportInterval, "r", reportIntervalDefault, "The interval between send of metrics to the server")
 	flag.DurationVar(&pollInterval, "p", pollIntervalDefault, "The interval between scrap of metrics")
 
