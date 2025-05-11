aae18f7a113e91227a353a055b57288f0738e3ec
Artem Makarevich <artemmakarevich1997@gmail.com>
2025-05-11 18:10:45 +0400

    iter5

--- cmd/agent/flag_test.go
+++ cmd/agent/flag_test.go
@@ -8,23 +8,61 @@ import (
 	"time"
 )
 
-func Test_configFlags_withDurations(t *testing.T) {
-	// Сброс флагов перед тестом
+func resetFlags() {
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+}
 
-	// Подставим аргументы
+func Test_configFlags_Priority(t *testing.T) {
+	t.Setenv(Adress_env, "https://env-address")
+	t.Setenv(ReportInterval_env, "99")
+	t.Setenv(PollInterval_env, "77")
+
+	resetFlags()
 	os.Args = []string{
 		"cmd",
-		"-a=127.0.0.1:9000",
-		"-r=15",
-		"-p=3",
+		"-a=cli-address",
+		"-r=33",
+		"-p=22",
 	}
 
-	// Вызов конфигурации
 	configFlags()
 
-	// Проверки
-	assert.Equal(t, "http://127.0.0.1:9000", serverAddress)
+	assert.Equal(t, "https://env-address", serverAddress)
+	assert.Equal(t, 99*time.Second, reportInterval)
+	assert.Equal(t, 77*time.Second, pollInterval)
+}
+
+func Test_configFlags_CLIOnly(t *testing.T) {
+	os.Unsetenv(Adress_env)
+	os.Unsetenv(ReportInterval_env)
+	os.Unsetenv(PollInterval_env)
+
+	resetFlags()
+	os.Args = []string{
+		"cmd",
+		"-a=myhost:5000",
+		"-r=15",
+		"-p=5",
+	}
+
+	configFlags()
+
+	assert.Equal(t, "http://myhost:5000", serverAddress)
 	assert.Equal(t, 15*time.Second, reportInterval)
-	assert.Equal(t, 3*time.Second, pollInterval)
+	assert.Equal(t, 5*time.Second, pollInterval)
+}
+
+func Test_configFlags_Defaults(t *testing.T) {
+	os.Unsetenv(Adress_env)
+	os.Unsetenv(ReportInterval_env)
+	os.Unsetenv(PollInterval_env)
+
+	resetFlags()
+	os.Args = []string{"cmd"}
+
+	configFlags()
+
+	assert.Equal(t, defaultServerAddress, serverAddress)
+	assert.Equal(t, time.Duration(reportIntervalDefault)*time.Second, reportInterval)
+	assert.Equal(t, time.Duration(pollIntervalDefault)*time.Second, pollInterval)
 }
